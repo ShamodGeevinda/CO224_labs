b394a181b3bda30e831c95e82de4ae4e488e7a2b
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2021-11-24 18:26:32 +0530

    lab2 done

--- lab2/lab2.s
+++ lab2/lab2.s
@@ -24,17 +24,33 @@ main:
 
 
 	@ ---------------------
-
+	mov r5,#0    	 @ sum = 0
+	mov r6,#0   	 @   i = 0
 	
+loop1:	cmp r6, #10   @ comparing i with 10
+		bge exit1
+		
+		mov r1, #5   			@ j = 5
+		loop2:  cmp r1, #15     @ comparing j with 15
+				bge exit2       @ exit from loop2(jump to exit2 label)
+				add r7, r6, r1  @ r7 = i+j
+				
+				cmp r7, #10  			 @ comparing with i +j<10
+				bge else      			 @ jump to else part
+				add r5, r5, r6, lsl #1   @ sum+=i*2
+				b exit3                  @ jump to exit3 label
+				
+				else: and r8, r6, r1     @ r8 = i & j
+				      add r5, r5, r8     @ sum += i&j
+				
+				exit3:  
+				add r1, r1, #1   @ j++
+				b loop2          @ calling loop2 again
+				exit2:
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	add r6,r6,#1   @ i++
+	b loop1;       @ calling loop1 again	
+	exit1:         @ exit from loop1
 	
 	
 	@ ---------------------
